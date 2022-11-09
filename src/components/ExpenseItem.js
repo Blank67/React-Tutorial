e2ce6ebb05f59221c9ca7a8cb3ae1de42f4efc35
Blank67 <hmnshshrm67@gmail.com>
2022-11-09 15:49:23 +0530

    use JS variables in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,24 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
+    const expenseDate = new Date().toLocaleDateString();
+    const expenseTitle = 'Food';
+    const expenseAmount = 10;
+    const locationOfExpenditure = 'Burger King'
+    
     return(
-        <div>
-            <h1>Expense Items</h1>
-            <h3>Food Rs 10</h3>
-            <h3>Petrol Rs 100</h3>
-            <h3>Movies Rs 200</h3>
+        <div className='expense-item'>
+            <div>Expense Items</div>
+            <div className='expense-item__description'>
+                <h2>{expenseDate}</h2>
+                <h2>{expenseTitle}</h2>
+                <h2>{locationOfExpenditure}</h2>
+                <div className='expense-item__price'>Rs {expenseAmount}</div>
+                {/* <h2>Petrol</h2>
+                <div className='expense-item__price'>Rs 100</div>
+                <h2>Movies</h2>
+                <div className='expense-item__price'>Rs 200</div> */}
+            </div>
         </div>
     );
 }
