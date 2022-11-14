1beb8ce4ded513faa03753bf9c0063bf7b11d4fa
Blank67 <hmnshshrm67@gmail.com>
2022-11-14 10:53:37 +0530

    creating multiple states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,17 +2,26 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 const ExpenseItem = (props) => {
 
-    const deleteHandler = (event) => {
-        console.log("Delete Button Clicked!!",event);
+    const [title,setTitle] = useState(props.title);
+    // const [amount,setAmount] = useState(props.amount);
+
+    // const changeAmountHandler = () => {
+    //     setAmount(100);
+    // }
+    const deleteHandler = () => {
+        setTitle('Updated title!');
+        console.log("Delete Button Clicked!!");
     };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate newDate={props.date} />
-            <ExpenseDetails newTitle = {props.title} newAmount = {props.amount}/>
+            <ExpenseDetails newTitle = {title} newAmount = {props.amount}/>
+            {/* <button onClick={changeAmountHandler}>Change Amount to 100</button> */}
             <button onClick={deleteHandler}>Delete Expense</button>
         </Card>
     );
