dd7974f5f6ac0b6baf9c25276e3efc38751cd600
Blank67 <hmnshshrm67@gmail.com>
2022-11-14 13:18:58 +0530

    alternative approach for multiple state handling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,21 +3,41 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
+    //Methdod 1
     const [enteredTitle,setEnteredTitle] = useState('');
     const [enteredAmount,setEnteredAmount] = useState('');
     const [enteredDate,setEnteredDate] = useState('');
+    //Method 2. Both are correct but we will use Method 1 
+    // const [userInput,setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     enteredDate:''
+    // });
 
     const titleChangeHandler = (e) => {
         // console.log(e.target.value);
         setEnteredTitle(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle:e.target.value
+        // });
+        // setUserInput((prevState) => { //We use this approach as in previous way if we have multiple states we won't get the latest previous state every time.
+        //     return {...userInput, enteredTitle:e.target.value};
+        // });
     }
     const amountChangeHandler = (e) => {
         console.log(e.target.value);
         setEnteredAmount(e.target.value);
+        // setUserInput((prevState) => {
+        //     return {...userInput, enteredAmount:e.target.value};
+        // });
     }
     const dateChangeHandler = (e) => {
         // console.log(e.target.value);
         setEnteredDate(e.target.value);
+        // setUserInput((prevState) => {
+        //     return {...userInput, enteredDate:e.target.value};
+        // });
     }
     
     return(
