0fcd750658fa1b8693eaeae62598d89864bbf7b8
Blank67 <hmnshshrm67@gmail.com>
2022-11-14 19:34:21 +0530

    adding missing files - filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,16 +8,16 @@ const ExpenseItem = (props) => {
 
     const [title,setTitle] = useState(props.title);
     
-    const deleteHandler = () => {
-        setTitle('Updated title!');
-        console.log("Delete Button Clicked!!");
-    };
+    // const deleteHandler = () => {
+    //     setTitle('Updated title!');
+    //     console.log("Delete Button Clicked!!");
+    // };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate newDate={props.date} />
             <ExpenseDetails newTitle = {title} newAmount = {props.amount}/>
-            <button onClick={deleteHandler}>Delete Expense</button>
+            {/* <button onClick={deleteHandler}>Delete Expense</button> */}
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
     // console.log(props.items);
+
+    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
         <Card className='expenses'>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {props.items.map((item, index) => {
                 return (
                     <ExpenseItem
